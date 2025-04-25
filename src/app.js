086ff61135fe6512d7b0aca8790f5168abb9a396
João <joaocarlosciclovida@gmail.com>
2025-04-25 19:24:38 -0300

    feat: feito configuracao no cors para aceitar apenas um cliente

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import appRouter from './app.routes.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({ path:'./.env' });
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENTE_ORIGIN,
+}));
 app.use('/api/v1', appRouter);
 
 export default app;
